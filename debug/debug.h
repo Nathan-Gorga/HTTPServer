dc2db4bd057f68967fd8277556574fb96d240c68
Nathan Gorga <gorga.nathan05@gmail.com>
2025-06-23 11:42:50 +0200

    also in debug

--- debug/debug.h
+++ debug/debug.h
@@ -6,11 +6,19 @@
 
 // #define DEBUG_MODE
 #define ASSERT_MODE
+#define FUNC_IN_PRINTF
 
 #ifdef DEBUG_MODE
 #define  PRINTF_DEBUG do{(void)printf("DEBUG : %s:%d:%s: \n",__FILE__,__LINE__,__func__); (void)fflush(stdout);}while(0);
 #else
 #define PRINTF_DEBUG
 #endif
+
+#ifdef FUNC_IN_PRINTF
+#define  PRINT_FUNC (void)printf(FUNC"%s"RESET" : ", __func__);
+#else
+#define PRINT_FUNC
+#endif
+
 
 #endif
