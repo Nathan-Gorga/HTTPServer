1121e138cd668c25d2b04087ea036942eeb5393d
Nathan Gorga <gorga.nathan05@gmail.com>
2025-06-23 19:12:47 +0200

    listen and respond to a request

--- src/main.c
+++ src/main.c
@@ -4,6 +4,35 @@
 
 #define VERBOSE true
 
+
+void handleClient(int client_socket) {
+    const int buffer_size = 4096;
+    char buffer[buffer_size] = {0};
+
+    ssize_t bytes_received = recv(client_socket, buffer, buffer_size - 1, 0);
+    if (bytes_received < 0) {
+        perror("recv failed");
+        close(client_socket);
+        return;
+    }
+
+    printf("---- Received HTTP Request ----\n%s\n", buffer);
+
+    const char *http_response =
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/plain\r\n"
+        "Content-Length: 13\r\n"
+        "\r\n"
+        "Hello, world!";
+
+    send(client_socket, http_response, strlen(http_response), 0);
+
+    close(client_socket);
+}
+
+
+
+
 int main (void){
 
     //DIVE IN DEPTH INTO THIS FUNCTION AND UNDERSTAND EVERY LINE
@@ -18,6 +47,27 @@ int main (void){
 
     if(VERBOSE)
         PRINT_FUNC(void)printf("socket %d listening...\n",socket_fd); 
+
+
+    while (1) {
+        struct sockaddr_in client_address;
+        socklen_t client_len = sizeof(client_address);
+
+        int client_socket = accept(socket_fd, (struct sockaddr*)&client_address, &client_len);
+        if (client_socket < 0) {
+            perror("accept failed");
+            continue;
+        }
+
+        if (VERBOSE) {
+            char client_ip[INET_ADDRSTRLEN];
+            inet_ntop(AF_INET, &client_address.sin_addr, client_ip, sizeof(client_ip));
+            printf("Accepted connection from %s:%d\n", client_ip, ntohs(client_address.sin_port));
+        }
+
+        handleClient(client_socket);
+    }
+
 
     if(close(socket_fd) != 0){
         perror("Error closing socket");
